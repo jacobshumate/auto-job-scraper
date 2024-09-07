1218137d573d7d219a2bec55280802686ae8e596
jacobshumate <shumatej.89@gmail.com>
2024-09-07 15:24:57 -0600

    Store path as variable

--- cron/run_scraper.sh
+++ cron/run_scraper.sh
@@ -6,11 +6,12 @@ SCRIPT_PATH=/home/scraperuser/app/main.py
 CONFIG_PATH=/home/scraperuser/app/data/config.json
 RESET_VPN=-reset_vpn
 LOG_PATH=/home/scraperuser/app/data/log/main.log
+ERROR_LOG_PATH=/home/scraperuser/app/data/log/error.log
 
 # Run the Python script and log the output
 $PYTHON_INTERPRETER $SCRIPT_PATH $CONFIG_PATH $RESET_VPN >> $LOG_PATH 2>&1
 
 # Optionally, you can add error handling or notifications here
 if [ $? -ne 0 ]; then
-  echo "Script failed at $(date)" >> /home/scraperuser/app/data/error.log
+  echo "Script failed at $(date)" >> $ERROR_LOG_PATH
 fi
