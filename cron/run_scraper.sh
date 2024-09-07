b00ceb147baa2deebb702a45fa91e7b4c11a78a6
jacobshumate <shumatej.89@gmail.com>
2024-09-07 16:11:49 -0600

    Add current time for when errors occur and add check if python script is already running.

--- cron/run_scraper.sh
+++ cron/run_scraper.sh
@@ -8,10 +8,19 @@ RESET_VPN=-reset_vpn
 LOG_PATH=/home/scraperuser/app/data/log/main.log
 ERROR_LOG_PATH=/home/scraperuser/app/data/log/error.log
 
+# Get current timestamp
+CURRENT_TIME=$(date '+%Y-%m-%d %H:%M:%S')
+
+# Check to see if Python script is already running, if it is, don't run and log
+if pidof -x python > /dev/null; then
+  echo "[$CURRENT_TIME] main.py is already running. Exiting script." >> $ERROR_LOG_PATH
+  exit 1
+fi
+
 # Run the Python script and log the output
 $PYTHON_INTERPRETER $SCRIPT_PATH $CONFIG_PATH $RESET_VPN >> $LOG_PATH 2>&1
 
 # Optionally, you can add error handling or notifications here
 if [ $? -ne 0 ]; then
-  echo "Script failed at $(date)" >> $ERROR_LOG_PATH
+  echo "[$CURRENT_TIME] Script failed at $(date)" >> $ERROR_LOG_PATH
 fi
